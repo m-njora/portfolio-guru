43aa12af96f349fa4efd8f18d284b11c23598039
m-njora <mmuthekijora@gmail.com>
2023-03-05 07:18:46 +0300

    user is now able to edit a skill

--- src/components/forms/SkillsForm.js
+++ src/components/forms/SkillsForm.js
@@ -3,24 +3,38 @@ import React, { useState } from 'react';
 function SkillsForm() {
   const [skills, setSkills] = useState([]);
   const [newSkill, setNewSkill] = useState('');
+  const [editingIndex, setEditingIndex] = useState(null);
   const MAX_SKILLS = 10;
-
   const handleNewSkillChange = (event) => {
     setNewSkill(event.target.value);
   };
 
   const handleAddSkill = (event) => {
     event.preventDefault();
-    if (skills.length < MAX_SKILLS) {
-      setSkills([...skills, newSkill]);
-      setNewSkill('');
+    if (editingIndex === null) {
+      if (skills.length < MAX_SKILLS) {
+        setSkills([...skills, newSkill]);
+        setNewSkill('');
+      } else {
+        alert(`You can't add more than ${MAX_SKILLS} skills`);
+      }
     } else {
-      alert(`You can't add more than ${MAX_SKILLS} skills`);
+      const newSkills = [...skills];
+      newSkills[editingIndex] = newSkill;
+      setSkills(newSkills);
+      setNewSkill('');
+      setEditingIndex(null);
     }
+  };
+
+  const handleEditSkill = (skillIndex) => {
+    setNewSkill(skills[skillIndex]);
+    setEditingIndex(skillIndex);
   };
 
   const handleDeleteSkill = (skillIndex) => {
     setSkills(skills.filter((_, index) => index !== skillIndex));
+    setEditingIndex(null);
   };
 
   return (
@@ -34,14 +48,25 @@ function SkillsForm() {
           value={newSkill}
           onChange={handleNewSkillChange}
         />
-        <button type="submit">Add Skill</button>
+        <button type="submit">{editingIndex !== null ? 'Save Skill' : 'Add Skill'}</button>
       </form>
       {skills.length > 0 ? (
         <ul>
           {skills.map((skill, index) => (
             <li key={index}>
-              {skill}
-              <button onClick={() => handleDeleteSkill(index)}>Delete</button>
+              {editingIndex === index ? (
+                <input type="text" value={newSkill} onChange={handleNewSkillChange} />
+              ) : (
+                <>{skill}</>
+              )}
+              {editingIndex === index ? (
+                <button onClick={() => setEditingIndex(null)}>Cancel</button>
+              ) : (
+                <>
+                  <button onClick={() => handleEditSkill(index)}>Edit</button>
+                  <button onClick={() => handleDeleteSkill(index)}>Delete</button>
+                </>
+              )}
             </li>
           ))}
         </ul>
@@ -53,7 +78,3 @@ function SkillsForm() {
 }
 
 export default SkillsForm;
-//In this example, we've added a constant MAX_SKILLS that specifies the maximum number of skills a user can add (in this case, 10). In the handleAddSkill function, we've added a check to ensure that the number of skills in the skills array is less than MAX_SKILLS before adding a new skill. If the number of skills is already at the maximum, we display an alert message to the user indicating that they cannot add any more skills.
-
-
-
